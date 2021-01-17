df8fdecf4397b4119355da9c283374e14d0f29a7
Lucas <lkslucas100@gmail.com>
2021-01-17 17:23:38 -0300

    site atualizado

--- function.js
+++ function.js
@@ -1,12 +1,12 @@
-let nav = document.getElementById('navbar-changing-color');
+// let nav = document.getElementById('navbar-changing-color');
 
 
-window.addEventListener('scroll', function (event) {
-    event.preventDefault();
-    if (window.scrollY <= nav.scrollHeight) {
-        nav.style.backgroundColor = 'transparent';
-    } else {
-        nav.style.backgroundColor = '#120032';
-        nav.style.transition = 'background-color 200ms linear'
-    }
-});
+// window.addEventListener('scroll', function (event) {
+//     event.preventDefault();
+//     if (window.scrollY <= nav.scrollHeight) {
+//         nav.style.backgroundColor = 'transparent';
+//     } else {
+//         nav.style.backgroundColor = 'black';
+//         nav.style.transition = 'background-color 200ms linear'
+//     }
+// });
