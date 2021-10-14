80445f79bb1eb92c3159fe2d7105e468dbc9c6be
CorreaArams <lkslucas100@gmail.com>
2021-10-14 11:37:52 +0100

    projeto em construção

--- function.js
+++ function.js
@@ -1,12 +1,32 @@
-// let nav = document.getElementById('navbar-changing-color');
+(function($) {
+    "use strict";
 
+    $('body').scrollspy({
+        target: '.fixed-top',
+        offset: 60
+    });
 
-// window.addEventListener('scroll', function (event) {
-//     event.preventDefault();
-//     if (window.scrollY <= nav.scrollHeight) {
-//         nav.style.backgroundColor = 'transparent';
-//     } else {
-//         nav.style.backgroundColor = 'black';
-//         nav.style.transition = 'background-color 200ms linear'
-//     }
-// });
+    $('#topNav').affix({
+        offset: {
+            top: 200
+        }
+    });
+    
+    new WOW().init();
+    
+    $('a.page-scroll').bind('click', function(event) {
+        var $ele = $(this);
+        $('html, body').stop().animate({
+            scrollTop: ($($ele.attr('href')).offset().top - 60)
+        }, 1450, 'easeInOutExpo');
+        event.preventDefault();
+    });
+    
+    $('.navbar-collapse ul li a').click(function() {
+        /* always close responsive nav after click */
+        $('.navbar-toggle:visible').click();
+    });
+
+    
+
+})(jQuery);
